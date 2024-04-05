a6002caba15251be5fe6ee2a5269c8af1d9b442b
Robert <robert.soor@gmail.com>
2024-04-05 18:11:45 +0200

    Added day 8 part 2

--- 08/wasteland.py
+++ 08/wasteland.py
@@ -1,4 +1,5 @@
-from typing import List, Optional, Dict
+from typing import Optional, Dict, List
+from math import lcm
 
 
 class Node:
@@ -6,7 +7,18 @@ class Node:
 
     ALL: Dict[str, "Node"] = {}  # "name": "node"
 
-    def __init__(self, name: str, left_name: Optional[str] = None, right_name: Optional[str] = None):
+    def __init__(
+        self,
+        name: str,
+        left_name: Optional[str] = None,
+        right_name: Optional[str] = None,
+    ):
+        if name is None or len(name) != 3:
+            raise ValueError("Name must be valid")
+
+        if name in self.ALL:
+            raise ValueError(f"Node `{name}` already registered!")
+
         self.name = name
         self.left_name = left_name
         self.right_name = right_name
@@ -33,40 +45,62 @@ class Node:
 
 
 def main():
-
-    with (open("input.txt", "r") as fh):
-
+    with open("input.txt", "r") as fh:
         directions_str = fh.readline().strip()
         fh.readline()
 
         while (line := fh.readline()) != "":
-
             node_name, _, line_rest = line.partition("=")
             line_rest = line_rest.strip().lstrip("(").rstrip(")")
             left_name, _, right_name = line_rest.partition(",")
-            node_name, left_name, right_name = node_name.strip(), left_name.strip(), right_name.strip()
+            node_name, left_name, right_name = (
+                node_name.strip(),
+                left_name.strip(),
+                right_name.strip(),
+            )
 
-            Node(node_name, left_name, right_name)  # No need to save reference to object
+            Node(
+                node_name, left_name, right_name
+            )  # No need to save reference to object
 
-    current_node = Node.ALL["AAA"]
+    current_node_names = [name for name in Node.ALL.keys() if name.endswith("A")]
     directions_index = 0
+    directions_len = len(directions_str)
     steps = 0
 
-    while current_node.name != "ZZZ":
-        if directions_str[directions_index] == "L":
-            current_node = current_node.left
-        else:
-            current_node = current_node.right
+    node_last_steps_on_z: List[Optional[int]] = [None] * len(current_node_names)
+    node_cycles: List[Optional[int]] = [None] * len(current_node_names)
+
+    # Keep walking through all the nodes until we've figured out the repeating patter
+    # for each starting node:
+
+    while any(n is None for n in node_cycles):
+        for i, current_node_name in enumerate(current_node_names):
+            if directions_str[directions_index] == "L":
+                current_node_names[i] = Node.ALL[current_node_name].left_name
+            else:
+                current_node_names[i] = Node.ALL[current_node_name].right_name
+
+            if current_node_names[i].endswith("Z"):
+                if node_cycles[i] is None:
+                    if node_last_steps_on_z[i] is not None:
+                        node_cycles[i] = steps - node_last_steps_on_z[i]
+                    node_last_steps_on_z[i] = steps
+
+        if steps > 10000000:
+            print(f"Aborted after {steps} steps...")
+            return
+            # raise RuntimeError(f"Steps exceeded {steps}, something is probably wrong")
 
         steps += 1
         directions_index += 1
-        if directions_index >= len(directions_str):
+        if directions_index >= directions_len:
             directions_index = 0
 
-        if steps > 100000:
-            raise RuntimeError("Steps getting too big, something is probably wrong")
+    print(f"Found pattern after steps: {steps}")
 
-    print(f"Total steps taken: {steps}")
+    expected_steps = lcm(*node_cycles)
+    print(f"Expect to get there after {expected_steps} steps")
 
 
 if __name__ == "__main__":
