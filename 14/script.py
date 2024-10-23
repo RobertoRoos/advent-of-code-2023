7f9247f1ca2211d7796bea237982965a6d84c76d
RobertoRoos <robert.soor@gmail.com>
2024-10-23 16:06:06 +0200

    Finished day 14 part 2 (but I'm embarrassed)

--- 14/script.py
+++ 14/script.py
@@ -1,14 +1,19 @@
 from dataclasses import dataclass
 from enum import Enum
-from typing import NamedTuple, Dict, Optional
-
-from pkg_resources import get_supported_platform
+from typing import NamedTuple, Dict, Optional, List
 
 
 class Tile(Enum):
     ROUND = "O"  # Not actually a normal zero
     CUBE = "#"
     EMPTY = "."
+
+
+class Direction(Enum):
+    NORTH = "north"
+    WEST = "west"
+    SOUTH = "south"
+    EAST = "east"
 
 
 class Coord(NamedTuple):
@@ -44,6 +49,39 @@ class Platform:
         print()
 
 
+def rotate_platform(platform: Platform, direction: Direction) -> Platform:
+    """Rotate a platform, assuming currently North is up.
+
+    Afterward the new direction is facing up.
+    """
+    new_platform = Platform()
+    rows_max = platform.rows - 1  # Highest indices
+    cols_max = platform.cols - 1
+    for coord, tile in platform.tiles.items():
+        if direction == Direction.WEST:
+            new_coord = Coord(row=coord.col, col=rows_max - coord.row)
+        elif direction == Direction.SOUTH:
+            new_coord = Coord(row=rows_max - coord.row, col=cols_max - coord.col)
+        elif direction == Direction.EAST:
+            new_coord = Coord(row=cols_max - coord.col, col=coord.row)
+        else:
+            new_coord = coord
+
+        new_platform.add_tile(new_coord.row, new_coord.col, tile)
+
+    return new_platform
+
+
+def cycle(platform: Platform) -> Platform:
+    """Perform a single cycle on a platform (four shifts)."""
+    for _ in range(4):
+        shift_up(platform)
+        platform = rotate_platform(platform, Direction.WEST)
+        # Actually we only need to rotate 90° CW each time
+
+    return platform  # By ending with a rotoation we got the original orientation back
+
+
 def shift_up(platform: Platform):
     """Shift all round boulders up as far as they'll go."""
     for col in range(platform.cols):
@@ -75,10 +113,38 @@ def get_weight(platform: Platform) -> int:
     return score
 
 
+def get_weight_after_many_cycles(platform: Platform, number: int) -> int:
+    """Keep repeating cycles for a set of times or until nothing changes."""
+    loop: List[Optional[int]] = [None]  # [ Index MOD loop size: Score }
+    loop_size = 1
+    repitions = 0  # Number of full loops passed
+    score = 0
+    for idx in range(number):
+        platform = cycle(platform)
+        score = get_weight(platform)
+
+        loop_idx = idx % loop_size
+
+        if idx > 20:
+            if any(x is None for x in loop):
+                loop[loop_idx] = score
+            else:
+                if loop[loop_idx] == score:
+                    if loop_idx == loop_size - 1:
+                        repitions += 1
+                else:
+                    loop_size += 1  # Make search bigger
+                    loop = [None] * loop_size  # And start again
+                    repitions = 0
+
+            if repitions >= 5:
+                return loop[number % loop_size - 1]
+
+    return score
+
+
 def main():
-
     platform = Platform()
-
     with open("input.txt", "r") as fh:
         row = 0
         while line := fh.readline():
@@ -93,11 +159,12 @@ def main():
 
     platform.print()
 
-    shift_up(platform)
+    # Part 1:
+    # shift_up(platform)
 
-    platform.print()
+    # Part 2:
+    score = get_weight_after_many_cycles(platform, 1000000000)
 
-    score = get_weight(platform)
     print("Score:", score)
 
 
